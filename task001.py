52d401fda2b71b4a88642128e931c0da8c6c5b69
git <st.uzver@gmail.com>
2018-09-28 17:03:38 +0300

    Some fixes

--- task001.py
+++ task001.py
@@ -5,20 +5,22 @@ import csv
 import argparse
 
 
+# Добавляем обработку аргументов командной строки
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--inputFile", help="--inputFile <FILE> - путь к CSV-файлу", action="store", required=True)
+parser.add_argument("-i", "--inputFile", help="--inputFile <FILE> - путь к CSV-файлу", required=True)
 parser.add_argument("-n", "--fieldName", help="--fieldName <NAME> - имя столбца (поля)", required=True)  # dataset for example
 parser.add_argument("-v", "--fieldValue", help="--fieldValue <VALUE> - значение столбца (поля)", required=True) # Outcomes for example
 args = parser.parse_args()
 
 
+# Функция для чтения и проверки csv файла
 def csv_reader(file):
     reader = csv.DictReader(file)
     counter = 0
     for row in reader:
         if args.fieldValue in row[args.fieldName]:
             counter += 1
-    print(counter)
+    print("There is", counter, args.fieldValue, "in", args.fieldName)
 
 try:
     with open(args.inputFile, newline='') as csvfile:
